6a8588c97447af634ff6b1f4eac892898190953e
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-14 15:18:23 +0500

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,341 @@
+// const developer = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+// };
+// console.log(developer);
+
+// const frontEndDev = {
+//   firstItem: "HTML",
+//   secondItem: "CSS",
+//   thirdItem: "JavaScript",
+// };
+// console.log(frontEndDev);
+
+// const backEndDev = {
+//   firstItem: "PYTHON",
+//   secondItem: "JAVA",
+//   thirdItem: "C#",
+// };
+// console.log(backEndDev);
+
+// const ages = {
+//   id: 1,
+//   age1: 17,
+//   items: {
+//     id2: 2,
+//     age2: 18,
+//     items: {
+//       id3: 3,
+//       age3: 19,
+//     },
+//   },
+// };
+// console.log(ages);
+
+// const developer = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   fatherName: "Bahodir",
+//   sex: "Male",
+//   dateOfBirth: 20102005,
+//   age: 17,
+//   telNumber: +998999954964,
+//   address: "Yashnobod tumani, Nodirabegim 99",
+//   job: "Developer, Trader",
+//   nationality: "O'zbek",
+//   hobby: "Kod yozish yoki trade qilish",
+// };
+// console.log(developer);
+
+// const nested = {
+//   name: "Qulmurod",
+//   surname: "Dilmurodov",
+//   fatherName: "Murod",
+//   address: {
+//     city: "Tashkent",
+//     district: "Yunusobod",
+//     streetName: "Temuriy",
+//   },
+//   hisNumbers: {
+//     telNum: +998999999999,
+//     homeNum: 99,
+//     passportNum: "UZ0000001",
+//     carNum: "01 777 ZZZ",
+//     cardNum: 8600001111001001,
+//   },
+//   jobOrWork: "Enteprenour or Taxi",
+//   nationality: "Uzbek",
+//   hobby: "sex",
+// };
+// console.log(nested);
+
+// const getValue = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+// };
+// console.log(getValue.name);
+// console.log(getValue.surname);
+// console.log(getValue.age);
+// console.log(getValue)
+
+// const developer = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+//   hello: function (num1, num2) {
+//     return num1 * num2;
+//   },
+// };
+// console.log(developer.hello(10, 10));
+// console.log(developer);
+
+// const developer = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+//   hello: function (number) {
+//     if (number > 0) {
+//       console.log(`${number} bu musbat son`);
+//     } else if (number === 0) {
+//       console.log(`${number} bu 0ga teng`);
+//     } else {
+//       console.log(`${number} bu manfiy son`);
+//     }
+//   },
+// };
+// console.log(developer.hello(1));
+// console.log(developer.hello(0));
+// console.log(developer.hello(-1));
+// console.log(developer);
+
+// const developer = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+// };
+// console.log(developer);
+// console.log(developer.name);
+// console.log(developer.surname);
+// console.log(developer.age);
+
+// const nested = {
+//   item: "1",
+//   item1: {
+//     item2: "2",
+//   },
+// };
+// console.log(nested);
+
+// const object = {
+//   name: "name is a key this is a keyValue",
+//   surname: "Shuhratullayev",
+//   age: 17,
+// };
+// console.log(object);
+// console.log(object.name);
+// console.log(object.surname);
+// console.log(object.age);
+
+// const nested = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+//   address: {
+//     city: "Toshkent",
+//     district: "Yashnobod",
+//     street: "Nodirabegim 99",
+//   },
+// };
+// console.log(nested);
+// console.log(nested.name);
+// console.log(nested.surname);
+// console.log(nested.age);
+// console.log(nested.address.city);
+// console.log(nested.address.district);
+// console.log(nested.address.street);
+
+// const object = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+// };
+// console.log(object);
+
+// const nested = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+//   address: {
+//     city: "Tashkent",
+//     district: "Yashnobod",
+//     street: "Nodirabegim 99",
+//   },
+// };
+// console.log(nested);
+// console.log(nested.name);
+// console.log(nested.surname);
+// console.log(nested.age);
+// console.log(nested.address.city);
+// console.log(nested.address.district);
+// console.log(nested.address.street);
+
+// const object = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+// };
+// console.log(object);
+// console.log(object.name);
+// console.log(object.surname);
+// console.log(object.age);
+
+// const nested = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+//   address: {
+//     city: "Tashkent",
+//     district: "Yashnobod",
+//     street: "Nodirabegim 99",
+//   },
+// };
+// console.log(nested);
+// console.log(nested.name);
+// console.log(nested.surname);
+// console.log(nested.age);
+// console.log(nested.address.city);
+// console.log(nested.address.district);
+// console.log(nested.address.street);
+
+// const object = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+// };
+// console.log(object);
+
+// const nested = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+//   addres: {
+//     city: "Tashkent",
+//     district: "Yashnobod",
+//     street: "Nodirabegim 99",
+//   },
+// };
+// console.log(nested);
+// console.log(nested.name);
+// console.log(nested.surname);
+// console.log(nested.age);
+// console.log(nested.addres.city);
+// console.log(nested.addres.district);
+// console.log(nested.addres.street);
+
+// const objectMethod = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+//   hello: function (password) {
+//     if (password === 999) {
+//       console.log("Parol to'g'ri");
+//     } else {
+//       console.log("Parol noto'g'ri qaytadan urinib ko'ring!");
+//     }
+//   },
+// };
+// console.log(objectMethod.hello(999));
+
+// const object = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+// };
+// console.log(object)
+
+// const nested = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+//   address: {
+//     city: "Tashkent",
+//     district: "Yashnobod",
+//     street: "Nodirabegim 99",
+//   },
+// };
+// console.log(nested);
+// console.log(nested.name);
+// console.log(nested.surname);
+// console.log(nested.age);
+// console.log(nested.address.city);
+// console.log(nested.address.district);
+// console.log(nested.address.street);
+
+// const objectMethod = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+//   hello: function (number) {
+//     if (number > 0) {
+//       console.log("Berilgan son 0dan katta!");
+//     } else if (number === 0) {
+//       console.log("Berilgan son 0ga teng!");
+//     } else {
+//       console.log("Berilgan son 0dan kichik!");
+//     }
+//   },
+// };
+// console.log(objectMethod.hello(1));
+// console.log(objectMethod.hello(0));
+// console.log(objectMethod.hello(-1));
+
+// const objects = [
+//   {
+//     name: "Akbar",
+//     surname: "Shuhratullayev",
+//     age: 17,
+//   },
+//   {
+//     name: "Akbarjon",
+//     surname: "Shuhratullayev",
+//     age: 19,
+//   },
+//   {
+//     name: "Akbarbek",
+//     surname: "Shuhratullayev",
+//     age: 21,
+//   },
+//   {
+//     name: "Akbarali",
+//     surname: "Shuhratullayev",
+//     age: 34,
+//   },
+//   {
+//     name: "Xislat",
+//     surname: "Shuhratullayev",
+//     age: 27,
+//   },
+// ];
+// console.log(objects);
+
+// const objectMethod = {
+//   name: "Akbar",
+//   surname: "Shuhratullayev",
+//   age: 17,
+//   hello: function (number) {
+//     if (number > 0) {
+//       console.log("Berilgan son 0dan katta!");
+//     } else if (number === 0) {
+//       console.log("Berilgan son 0ga teng!");
+//     } else {
+//       console.log("Berilgan son 0dan kichik!");
+//     }
+//   },
+// };
+// console.log(objectMethod.hello(1));
+// console.log(objectMethod.hello(0));
+// console.log(objectMethod.hello(-1));
+
+
