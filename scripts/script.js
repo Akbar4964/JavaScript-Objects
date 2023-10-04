8957a8582ce25d88baa7b22b7d141ffb8b7da181
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-10-04 10:51:50 +0500

    added new codes

--- scripts/script.js
+++ scripts/script.js
@@ -576,5 +576,4 @@ family4.isLarge = true;
 family4.man.name = "Baxodir";
 family4.man.child.age = 18;
 family4.man.child.baby.name = "Baqaloq";
-
 // console.log(family.man.child.baby, family4.man.child.baby);
