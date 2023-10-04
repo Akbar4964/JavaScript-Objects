7164ceb5572edd08764e1145117183821e69189f
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-10-04 10:45:05 +0500

    added new codes

--- scripts/script.js
+++ scripts/script.js
@@ -519,7 +519,7 @@ console.log(person["job"]);
 
 const { name, age, job } = person;
 
-// const [meva, meva2, meva3, meva4] = mevalar;
+const [meva, meva2, meva3, meva4] = mevalar;
 
 // const name = "Akbar";
 
