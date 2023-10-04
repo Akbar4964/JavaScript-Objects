f3c72428f8916c5fa74e6cd6732cde3b37f3b303
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-10-04 10:50:31 +0500

    added new codes

--- scripts/script.js
+++ scripts/script.js
@@ -568,8 +568,8 @@ family2.man.child.age = 10;
 
 family3.members = 10;
 family3.isLarge = true;
-// family3.man.name = "Ali";
-// family3.man.child.age = 14;
+family3.man.name = "Ali";
+family3.man.child.age = 14;
 
 // family4.members = 15;
 // family4.isLarge = true;
